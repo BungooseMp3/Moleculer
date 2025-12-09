5602088db1f1f8b736fe93495f186d63dd6c4325
BungooseMp3 <zackie.woolhouse@gmail.com>
2025-12-09 09:20:31 +0000

    Added second version on the halogen selection menu - very laggy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,12 @@ import UI.MainWindow;
 import UI.MoleculeComponent;
 import UI.Workspace;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        JPopupMenu.setDefaultLightWeightPopupEnabled(true);
         MainWindow window = new MainWindow();
     }
 }
--- src/main/java/UI/UIButtons/HalogenButton.java
+++ src/main/java/UI/UIButtons/HalogenButton.java
@@ -30,13 +30,24 @@ public class HalogenButton extends TextButton {
     @Override
     public void buttonPressed() {
         this.getWorkspace().setCurrentTool(new ElementPlacer(this.getText(), this.getWorkspace()));
-        menu.show(this,0,menu.getHeight());
+        menu.show(this,0,0);
     }
 
     public class HalogenMenu extends JPopupMenu {
         public HalogenMenu(Workspace workspace,HalogenButton outerButton){
             this.add(new HalogenGrid(workspace,this, outerButton));
+            this.setBackground(new Color(190,10,10,255));
         }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        g2.fillPolygon(new Polygon(new int[]{this.getWidth(),this.getWidth(),(int)(this.getWidth()*0.7 )},new int[]{this.getHeight(),(int)(this.getHeight()*0.7),this.getHeight()},3));
+
     }
 
     public class HalogenGrid extends GridTemplate{
@@ -61,10 +72,10 @@ public class HalogenButton extends TextButton {
             });
 
             this.initGrid(2,2,new Dimension(100,100),10,new Color(190,10,10,255),workspace);
-            this.add(new HalogenGridButton("F", 0.75,20,workspace,outerButton));
-            this.add(new HalogenGridButton("Cl", 0.75,20,workspace,outerButton));
-            this.add(new HalogenGridButton("Br", 0.75,20,workspace,outerButton));
-            this.add(new HalogenGridButton("I", 0.75,20,workspace,outerButton));
+            this.add(new HalogenGridButton("F", 0.75,20,workspace,outerButton,menu));
+            this.add(new HalogenGridButton("Cl", 0.75,20,workspace,outerButton,menu));
+            this.add(new HalogenGridButton("Br", 0.75,20,workspace,outerButton,menu));
+            this.add(new HalogenGridButton("I", 0.75,20,workspace,outerButton,menu));
             this.setOpaque(true);
         }
     }
@@ -72,8 +83,10 @@ public class HalogenButton extends TextButton {
     public class HalogenGridButton extends TextButton{
 
         HalogenButton outerButton;
+        HalogenMenu menu;
 
-        public HalogenGridButton(String text,double fontScale,double maxSize,Workspace workspace,HalogenButton outerButton){
+        public HalogenGridButton(String text,double fontScale,double maxSize,Workspace workspace,HalogenButton outerButton, HalogenMenu menu){
+            this.menu = menu;
             this.initTextButton(text,fontScale,maxSize,workspace);
             this.outerButton = outerButton;
         }
@@ -82,6 +95,8 @@ public class HalogenButton extends TextButton {
         public void buttonPressed() {
             this.getWorkspace().setCurrentTool(new ElementPlacer(this.getText(), this.getWorkspace()));
             outerButton.setText(this.getText());
+            menu.setVisible(false);
+            this.repaint();
         }
     }
 }
